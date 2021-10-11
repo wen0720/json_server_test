8481cdcfdf3e0fbfbd30de99d5aaca3714610194
wen Tim <wentim@wende-MacBook-Pro.local>
2021-10-11 21:17:30 +0800

    add json-server-extension

--- server/index.js
+++ server/index.js
@@ -3,53 +3,75 @@
 
 const path = require('path');
 const jsonServer = require('json-server');
+const _jsonExtender = require('json-server-extension');
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-const middlewares = jsonServer.defaults();
+//options:
+//fullPath:fullpath for the combined object
+//generatedPath:the path where the generated files will be found
+//staticPath:the path where the static files will be found
+const jsonExtender = new _jsonExtender({filePath: path.join(__dirname, './db_extends.json'),
+                                        generatedPath: path.join(__dirname, './generated'),
+                                        staticPath: path.join(__dirname,'static')});
 
-// const { db } = router;
+//register accacpt array of generators or path to the generator scripts
+//const funcs =  Object.keys(generators).map(key => generators[key])
+jsonExtender.register(path.join(__dirname, 'generators'));
+jsonExtender.generate().then((data)=>{
+  /* 產生檔案完畢，開始啟動 json-server */
 
-// 定義回傳格式
-router.render = (req, res) => {
-  res.jsonp({
-    succeeded: true,
-    msg: null,
-    data: res.locals.data,
+  const server = jsonServer.create();
+  const router = jsonServer.router(path.join(__dirname, 'db.json'));
+  const middlewares = jsonServer.defaults();
+
+  // const { db } = router;
+  // 定義回傳格式
+  router.render = (req, res) => {
+    res.jsonp({
+      succeeded: true,
+      msg: null,
+      data: res.locals.data,
+    });
+  };
+
+  // Set default middlewares (logger, static, cors and no-cache)
+  server.use(middlewares);
+
+  // To handle POST, PUT and PATCH you need to use a body-parser
+  // You can use the one used by JSON Server
+  server.use(jsonServer.bodyParser);
+  server.use((req, res, next) => {
+    if (req.method === 'POST') {
+      req.body.createdOn = +new Date();
+    }
+    // Continue to JSON Server router
+    next();
   });
-};
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
+  // 於此之後，/api 都會拿掉，並保留原本路徑
+  server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+  }));
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdOn = +new Date();
-  }
-  // Continue to JSON Server router
-  next();
-});
+  server.get('/error', (req, res) => {
+    res.status(401).json({
+      succeeded: 'ERROR',
+      msg: '',
+      data: null,
+    }).end();
+  });
 
-// 於此之後，/api 都會拿掉，並保留原本路徑
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}));
+  server.use(router);
+  server.listen(3001, () => {
+    console.log('====== JSON SERVER START AT PORT 3001 =======');
+  });
 
-server.get('/error', (req, res) => {
-  res.status(401).json({
-    succeeded: 'ERROR',
-    msg: '',
-    data: null,
-  }).end();
-});
+  /* 產生檔案完畢，開始啟動 json-server */
 
-server.use(router);
-server.listen(3001, () => {
-  console.log('====== JSON SERVER START AT PORT 3001 =======');
-});
+}).catch((error) => {
+  console.log(error.message, 'red')
+})
+
+
 
 // server.post('/:apiType', async (req, res, next) => {
 //   const list = db.getState()[req.params.apiType];
